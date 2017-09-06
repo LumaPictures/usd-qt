6f601545f15d1041e3d4d197bada1c9c948bb6f2
Matthew Kuruc <mkuruc@pixar.com>
2017-09-06 10:31:12 -0700

    -- adding missing undo python bindings -- fixing url for setup.py

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     version='0.0.1',
     description='USD Qt Components',
     long_description=long_description,
-    url='https://github.com/LumaPictures/usdqt-components',
+    url='https://github.com/LumaPictures/usd-qt',
     license='Modified Apache 2.0 License',
     packages=packages,
     package_dir={'UsdQt':'usdQt'},
