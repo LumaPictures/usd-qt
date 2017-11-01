526ac258d3bb36c09aec374c82daf710b08e6b73
Paul Molodowitch <pm@stanfordalumni.org>
2017-11-01 11:23:57 -0700

    al_utils.getProxyShape: get rid of unnecessary proxyShape arg, add error arg

--- usdQt/thirdparty/al_utils.py
+++ usdQt/thirdparty/al_utils.py
@@ -9,6 +9,10 @@ import pymel.core as pm
 AL_MAYA_PLUGIN = 'AL_USDMayaPlugin'
 
 
+class NoProxyShapeError(ValueError):
+    pass
+
+
 def loadAndImportALUsdMaya():
     if not pm.pluginInfo(AL_MAYA_PLUGIN, q=1, loaded=1):
         pm.loadPlugin(AL_MAYA_PLUGIN)
@@ -16,25 +20,34 @@ def loadAndImportALUsdMaya():
     return AL
 
 
-def getProxyShape(proxyShape=None):
-    '''Return a proxyShape to use for a ui given the current maya context'''
-    if proxyShape is None:
-        sel = []
-        for node in pm.ls(selection=True):
-            if type(node) == pm.nt.Transform:
-                node = node.getShape()
-            if node and node.type() == 'AL_usdmaya_ProxyShape':
-                sel.append(node)
-
-        if sel:
-            proxyShape = sel[0]
+def getProxyShape(error=True):
+    '''Return a proxyShape to use for a ui given the current maya context
+    
+    Will first attempt to find the first proxy shape within the current
+    selection, and if none are found there, will check to see if exactly one
+    exists in the scene.
+    
+    Parameters
+    ----------
+    error : bool
+        if False, and we can't find a (singular) proxyShape, return None;
+        otherwise, a NoProxyShapeError will be raised
+    '''
+    proxyShape = None
+    for node in pm.ls(orderedSelection=True):
+        if type(node) == pm.nt.Transform:
+            node = node.getShape()
+        if node and node.type() == 'AL_usdmaya_ProxyShape':
+            proxyShape = node
+            break
     if proxyShape is None:
         shapes = pm.ls(type='AL_usdmaya_ProxyShape')
         if len(shapes) == 1:
             proxyShape = shapes[0]
-    if proxyShape is None:
-        raise ValueError('Could not resolve a single AL_usdmaya_ProxyShape '
-                         'node in the current scene.')
+    if proxyShape is None and error:
+        raise NoProxyShapeError(
+            'Could not resolve a single AL_usdmaya_ProxyShape node in the '
+            'current selection / selection.')
     return proxyShape
 
 
